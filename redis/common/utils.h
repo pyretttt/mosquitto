295d91dc0f891aa1e7646ecac2d1cf9bb5845644
s-a-bakulin <s-a-bakulin@yandex-team.ru>
2024-09-18 11:08:23 +0500

    non blocking fd

--- redis/common/utils.h
+++ redis/common/utils.h
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <fcntl.h>
 
-static inline int32_t read_full(int fd, char *buff, size_t n) {
+inline int32_t read_full(int fd, char *buff, size_t n) {
     while (n > 0) {
         ssize_t rv = read(fd, buff, n);
         if (rv <= 0) {
@@ -16,7 +17,7 @@ static inline int32_t read_full(int fd, char *buff, size_t n) {
 }
 
 
-static inline int32_t write_all(int fd, const char *buff, size_t n) {
+inline int32_t write_all(int fd, const char *buff, size_t n) {
     while (n > 0) {
         ssize_t rv = write(fd, buff, n);
         if (rv <= 0) {
@@ -27,4 +28,21 @@ static inline int32_t write_all(int fd, const char *buff, size_t n) {
         buff += rv;
     }
     return 0;
+}
+
+inline void fd_to_nb(int fd) {
+    errno = 0;
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (errno) {
+        std::cerr << "Failed to get fd flags" << std::endl;
+        return;
+    }
+
+    flags |= O_NONBLOCK;
+
+    errno = 0;
+    fcntl(fd, F_SETFL, flags);
+    if (errno) {
+        std::cerr << "Failed to set fd flags" << std::endl;
+    }
 }
