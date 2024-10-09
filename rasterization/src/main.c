4229346f3cfc0926c44a5b804319f4d9849f1f7f
pyretttt <percalee@gmail.com>
2024-10-09 21:25:15 +0400

    rasterization

--- rasterization/src/main.c
+++ rasterization/src/main.c
@@ -18,6 +18,16 @@ vec2_t vertices[5] = {
   { .x = 75, .y = 20 },
 };
 
+typedef struct {
+  uint8_t r, g, b;
+} color_t;
+
+color_t colors[3] = {
+  {0xFF, 0x00, 0x00},
+  {0x00, 0xFF, 0x00},
+  {0x00, 0x00, 0xFF},
+};
+
 void process_input(void) {
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
@@ -47,6 +57,7 @@ int edge_cross(vec2_t* a, vec2_t* b, vec2_t* p) {
 }
 
 void triangle_fill(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
+  
   // Finds the bounding box with all candidate pixels
   int x_min = MIN(MIN(v0.x, v1.x), v2.x);
   int y_min = MIN(MIN(v0.y, v1.y), v2.y);
@@ -57,6 +68,8 @@ void triangle_fill(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
   int bias0 = is_top_left(&v1, &v2) ? 0 : -1;
   int bias1 = is_top_left(&v2, &v0) ? 0 : -1;
   int bias2 = is_top_left(&v0, &v1) ? 0 : -1;
+
+  float area = edge_cross(&v0, &v1, &v2);
 
   // Loop all candidate pixels inside the bounding box
   for (int y = y_min; y <= y_max; y++) {
@@ -69,7 +82,21 @@ void triangle_fill(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
       bool is_inside = w0 >= 0 && w1 >= 0 && w2 >= 0;
 
       if (is_inside) {
-        draw_pixel(x, y, color);
+        float alpha = w0 / area;
+        float beta = w1 / area;
+        float gamma = w2 / area;
+
+        int a = 0xFF;
+        int r = (alpha) * colors[0].r + (beta) * colors[1].r + (gamma) * colors[2].r;
+        int g = (alpha) * colors[0].g + (beta) * colors[1].g + (gamma) * colors[2].g;
+        int b = (alpha) * colors[0].b + (beta) * colors[1].b + (gamma) * colors[2].b;
+        uint32_t interpolated_color = 0x00000000;
+        interpolated_color = (interpolated_color | a) << 8;
+        interpolated_color = (interpolated_color | b) << 8;
+        interpolated_color = (interpolated_color | g) << 8;
+        interpolated_color = (interpolated_color | r);
+
+        draw_pixel(x, y, interpolated_color);
       }
     }
   }
