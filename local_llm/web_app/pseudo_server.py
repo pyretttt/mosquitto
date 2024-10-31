d8f50396b769f93926d82baee2b0fc2633ff438d
Jack Sgory <percalee@gmail.com>
2024-10-31 16:51:13 +0500

    web_server

--- local_llm/web_app/pseudo_server.py
+++ local_llm/web_app/pseudo_server.py
@@ -26,150 +26,45 @@ class Datasink:
     is_done: () -> bool
     done_with_trailed: (Headers) -> bool
 
+# Common
+def create_client_socket(scheme, host, port):
+    famility, type, proto = ... # from scheme, host, port
+    sock = socket(famility, type, proto)
+    ... # setup_socket() - set flags and modes
+    return sock
+
+
 ## HTTP Client
-class HTTPClient:
-    def __init__(self, **kwargs) -> None:
-        host_and_port = adjust_ipv6(kwargs["host"]) + ":" + kwargs["port"]
-        client_cert_path = client_cert_path
-
-    def copy_settings(self, rhs):
-        ...
+class HttpClient:
+    def __init__(self, scheme_and_port: str):
+        self.scheme_and_port = scheme_and_port
+        self.scheme, self.host, self.port = ... # <parse_link>
+        self.recv_timeout = ... # default
+        self.send_timeout = ... # default
+        self.keep_connection_alive = ... # default
     
-    def create_socket():
-        if has_proxy_host:
-            create_socket(proxy_host, proxy_port, socket_args...)
+    def create_and_connect_client_socket(self):
+        sock = create_client_socket(self.scheme, self.host, self.port)
+        ai_addr = ... # from scheme, host, port
+        ... # bind(sock, ai_addr)
+        ... # set_nonblocking(sock, true)
+        ... # connect(sock, ai_addr)
+        ... # set_timeouts(sock, self.recv_timeout, self.send_timeout)
 
-        ip = check_ip_overwrites(host)
-        return create_socket(proxy_host, proxy_port, ip, socket_args...)
+        ... # return Socket(sock)
     
-    def create_and_connect_socket():
-        return Socket(create_socket())
+    def shutdown_socket(self, socket: Socket):
+        ... # shutdown(socket.socket, SHUT_RDWR)
     
-    def read_response_line(Stream strm, req, response):
-        line = read_line(strm)
-        regex = r"(HTTP/1\\.[01]) (\\d{3})(?: (.*?))?\r\n" # Looking for http header
-        smatch = line.match(regex)
-
-        if smatch is None:
-            return req.method == "Connect"
+    def send(self, req, res):
+        is_alive = ... # check_whether_socket_is_alive(self.sock)
+        if not is_alive:
+            ... # turn_off_socket(self.sock)
+            self.sock = self.create_and_connect_client_socket()
         
-        res.version = smatch.group(1)
-        res.status = smatch.group(2)
-        res.reason = smatch.group(3)
+        ... # setup_default_headers(req)
+        self.handle_request(req, res)
 
-        while (res.status = StatusCode::Continue_100): # Response buffered already or not
-            if !read_line(strm): return False # CRLF
-            next_response_line = read_line(strm)
-            if next_response_line is None: return False
-
-            smatch = line.match(next_response_line)
-            if smatch is None:
-                return False
-            res.version = smatch.group(1)
-            res.status = smatch.group(2)
-            res.reason = smatch.group(3)
-
-        return True
-    
-    def send(req, resp):
-        with request_lock:
-            with socket_mutex:
-                self.socket_should_be_closed_when_request_is_done_ = False
-                is_alive = False
-                if sock.is_open():
-                    is_alive = self.sock.alive()
-                    if !is_socket_alive:
-                        shutdown_ssl(socket)
-                        shutdown_socket(socket)
-                        close_socket(socket)
-
-                if !is_alive:
-                    if !create_and_connect_socket(sock):
-                        return False
-                
-                socket_requests_in_flight++
-                socket_requests_are_from_thread_ = thread.id()
-            
-            req.set_default_headers_if_not_set()
-            should_close_connection = !self.keep_alive
-
-            ret = 0
-            defer {
-                with socket_mutex:
-                    socket_requests_in_flight--;
-                    if should_close_connection || ret:
-                        shutdown_ssl(socket)
-                        shutdown_socket(socket)
-                        close_socket(socket)
-            }
-
-            handle_request(req, resp, should_close_connection)
-
-    def write_content_with_provider(strm, req):
-        if req.is_chunked_content_provider:
-
-
-    def write_request(strm, req, close_connection):
-        if close_connection:
-            req.set_header_if_not("Connection", "close")
-        
-        req.set_header_if_not("Host", host_ or host_and_port)
-        req.set_header_if_not("Accept", "*/*")
-        
-        if req.body.isEmpty():
-            if req.content_provider:
-                if !req.content_provider.chunked:
-                    req.set_header_if_not("Content-Length", req.content_length)
-            else:
-                if req.method in ("POST", "PUT", "PATCH"):
-                    req.set_header_if_not("Content-Length", 0)
-        else:
-            req.set_header_if_not("Content-Type", "text/plain")
-            req.set_header_if_not("Content-Length", req.body.size())
-
-        if !basic_auth_password.isEmpty() or !basic_auth_username.isEmpty():
-            auth_header = f"Basic {base64_encode(basic_auth_username + ":" basic_auth_password)}"
-            req.set_header_if_not("Proxy-Authorization" if is_proxy else "Authorization", auth_header)
-
-        if !bearer_token.isEmpty():
-            auth_header = f"Bearer {bearer_token}"
-            req.set_header_if_not("Proxy-Authorization" if is_proxy else "Authorization", auth_header)
-        
-        if !bearer_token_proxy.isEmpty():
-            auth_header = f"Bearer {bearer_token_proxy}"
-            req.set_header_if_not("Proxy-Authorization", auth_header)
-
-        path = url_encode(req.path) if decode_url else req.path
-        stream = io.StringIO()
-        stream.write(f"{req.method} {req.path} HTTP/1.1\r\n")
-        for k, v in req.headers.items():
-            stream.write(f"{k}: {v}\r\n")
-        
-        strm.write(stream.getvalue()) # actually writes to socket
-
-        # body
-        if req.body.isEmpty():
-            return write_content_with_provider(strm, req)
-    
-        if req.body.isEmpty():
-            return strm.write(req.body)
-        
-        return True
-
-
-    def process_request(strm, req, resp, close_connection):
-
-
-    def handle_request(req, resp, close_connection):
-        sock_stream = socket_stream(sock, timeouts...)
-        if req.path.empty():
-            raise error
-        
-        if !ssl:
-            req.path = "http://" + host_and_port + req.path
-            process_request(strm, req, res, close_connection)
-        else:
-            process_request(strm, req, res, close_connection)
-
-def write_content_chunked(strm, content_provider, is_shutting_down, compressor):
-    
+    def handle_request(self, req, res):
+        req.path = ... # make path from self.host, self.port, req.path
+        # TODO
