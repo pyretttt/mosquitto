434a05c5835ef885ce9df57f847d7ad724813b76
pyretttt <percalee@gmail.com>
2024-11-09 10:40:51 +0400

    i

--- opengl_playground/src/main.cpp
+++ opengl_playground/src/main.cpp
@@ -3,13 +3,6 @@
 #include "glad/gl.h"
 #include "GLFW/glfw3.h"
 
-
-struct T {
-    static std::string name;
-};
-
-std::string T::name = "Jack";
-
 int main() {
     GLFW_ACCUM_ALPHA_BITS;
     glfwDefaultWindowHints();
