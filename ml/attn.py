3c8ba4ffb9f55cd6ad4e76f01d980df0f6bd941f
pyretttt <percalee@gmail.com>
2025-01-20 19:22:48 +0400

    attn

--- ml/attn.py
+++ ml/attn.py
@@ -1,3 +1,5 @@
+import math
+
 import torch, torch.nn as nn
 
 
@@ -11,7 +13,8 @@ class Attention(nn.Module):
         input_size: int, 
         d_k: int,
         bias: bool = True,
-        is_self_attn: bool = False
+        is_self_attn: bool = False,
+        dropout: float = 0.2
     ):
         super().__init__()
         self.d_k = d_k
@@ -21,6 +24,8 @@ class Attention(nn.Module):
         self.values_proj = nn.Linear(input_size, d_k, bias=bias) # Weights with shape (d_k x I)
         self.linear = nn.Linear(d_k, input_size, bias=bias)
         self.is_self_attn = is_self_attn
+        self.attn_dropout = nn.Dropout(dropout, inplace=True)
+        self.resid_dropout = nn.Dropout(dropout, inplace=True)
         
     
     def init_keys(self, x: torch.Tensor):
@@ -44,15 +49,16 @@ class Attention(nn.Module):
         
         attn_scores = torch.matmul(
             queries, 
-            torch.permute(self.keys, (0, 2, 1))
-        ) / (self.d_k ** 0.5) # (B x S_source x S_target)
+            self.keys.transpose(-2, -1)
+        ) / math.sqrt(self.d_k) # (B x S_source x S_target)
         if mask:
             attn_scores.masked_fill(mask == 0, float('-inf'))
         
-        attn_scores = torch.softmax(attn_scores, dim=-2) # over source sequence
+        attn_scores = torch.softmax(attn_scores, dim=-1) # over source sequence
+        attn_scores = self.attn_dropout(attn_scores)
         
         ctx_sequence = torch.matmul(attn_scores, self.values) # B x S x d_k
-        out = self.linear(ctx_sequence) # B x S x F
+        out = self.resid_dropout(self.linear(ctx_sequence)) # B x S x F
         
         return out
 
@@ -64,7 +70,8 @@ class MultiheadAttention(nn.Module):
         d_k: int,
         nheads: int,
         bias: bool = True,
-        is_self_attn: bool = False
+        is_self_attn: bool = False,
+        dropout: float = 0.2
     ):
         super().__init__()
         assert d_k % nheads == 0, "KVS dimension should be divisible by number of attention heads"
@@ -77,6 +84,9 @@ class MultiheadAttention(nn.Module):
         self.values_proj = nn.Linear(input_size, d_k, bias=bias) # Weights with shape (d_k x I)
         self.linear = nn.Linear(d_k, input_size)
         self.is_self_attn = is_self_attn
+        self.attn_dropout = nn.Dropout(dropout, inplace=True)
+        self.resid_dropout = nn.Dropout(dropout, inplace=True)
+        self.std = math.sqrt(self.d_k // self.nheads)
         
     
     def init_keys(self, x):
@@ -92,8 +102,7 @@ class MultiheadAttention(nn.Module):
         # (B x S x d_k) ~> (B x H x S x d_k / H)
         B, S, _ = x.shape
         out = kvs(x)
-        out = torch.reshape(out, (B, S, self.nheads, int(self.d_k // self.nheads)))
-        out = out.permute(0, 2, 1, 3)
+        out = out.view(B, S, self.nheads, self.d_k // self.nheads).transpose(1, 2)
         return out
     
     def forward(self, x: torch.Tensor, mask: torch.Tensor = None):
@@ -106,22 +115,25 @@ class MultiheadAttention(nn.Module):
         Returns:
             torch.Tensor: shape (B x S x F)
         """
+        B, S, _ = x.size()
         queries = self.multihead_proj(self.queries_proj, x) # (B x H x S x d_k // n_heads)
         if self.is_self_attn:
             self.init_keys(x)
+
         attn_scores = torch.matmul(
-            queries, 
-            torch.permute(self.keys, (0, 1, 3, 2))
-        ) / ((self.d_k // self.nheads) ** 0.5) # (B x H x S_source x S_target)
+            queries,
+            self.keys.transpose(-2, -1)
+        ) / (self.std) # (B x H x S_source x S_target)
         
         if mask:
             attn_scores.masked_fill(mask == 0, float('-inf'))
 
-        attn_scores = torch.softmax(attn_scores, dim=-2) # over source sequence
+        attn_scores = torch.softmax(attn_scores, dim=-1) # over source sequence
+        attn_scores = self.attn_dropout(attn_scores)
 
         ctx_sequence = torch.matmul(attn_scores, self.values) # B x H x S x d_k // n_heads
-        ctx_sequence = ctx_sequence.permute(0, 2, 1, 3).flatten(-2) # B x S x d_k
-        out = self.linear(ctx_sequence) # B x S x F
+        ctx_sequence = ctx_sequence.transpose(1, 2).contiguous().view(B, S, self.d_k) # B x S x d_k
+        out = self.resid_dropout(self.linear(ctx_sequence)) # B x S x F
         
         return out
 
