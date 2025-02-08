cff8f68d44e863ecf667f7b55764e0cb695dac6e
Jack Sgory <percalee@gmail.com>
2025-02-08 18:08:41 +0500

    engine

--- godot_playground/fight_engine.gd
+++ godot_playground/fight_engine.gd
@@ -3,11 +3,43 @@ class_name FightEngine
 
 extends RefCounted
 
-var characters: Dictionary
+var characters: Dictionary # Dictionary[Team, Array[Characters]]
 var operations: Dictionary # Dictionary[Character, Array[AtomOp]]
 
 #func _init():
+
+func loop(dt: float):
+	var is_char_alive = func (char: Character):
+		char.current_hp > 0
+	var red_team: Array[Character] = characters[Team.RED]
+	var blue_team: Array[Character] = characters[Team.BLUE]
+	var red_alive = red_team.filter(is_char_alive)
+	var blue_alive = blue_team.filter(is_char_alive)
 	
+	for character: Character in operations: # It implies that all actions are simultaneous
+		var ops: Array[AtomOp] = operations[character]
+		var to_append: Array[AtomOp] = []
+		var to_remove_idx: Array[int] = []
+		for op_idx in range(ops.size()):
+			var op = ops[op_idx]
+			to_append.append_array(op.tick(dt, characters))
+			if op.progress >= 1.0:
+				to_remove_idx.append(op_idx)
+			if op.is_exclusive:
+				break
+		
+		to_remove_idx.reverse()
+		for remove_idx in to_remove_idx:
+			ops.remove_at(remove_idx)
+		ops.append_array(to_append)
+
+	for character: Character in red_alive:
+		var actions = character.act(characters)
+		operations[character] += actions
+	
+	for character: Character in blue_alive:
+		var actions = character.act(characters)
+		operations[character] += actions
 
 ## FIGHT ENGINE END
 
@@ -22,23 +54,30 @@ class Pair:
 		self.right = right
 
 class AtomOp:
-	var owner: WeakRef
+	var owner: Character
 	var duration: float
 	var progress: float
 	var is_exclusive: bool
+	var is_interruptable: bool
+	
+	var owner_id: int:
+		get: return owner.id
+
 	var is_owner_alive: bool:
-		get: return owner.get_ref() != null
+		get: return owner.current_hp > 0
 	
 	func _init(
-		owner: WeakRef,
+		owner: Character,
 	 	duration: float,
 		progress: float = 0.0,
-		is_exclusive: bool = true
+		is_exclusive: bool = true,
+		is_interruptable: bool = true
 	):
 		self.owner = owner
 		self.duration = duration
 		self.progress = progress
 		self.is_exclusive = is_exclusive
+		self.is_interruptable = is_interruptable
 
 	func update_progress(dt: float): # in seconds
 		var delta_progress = dt / duration
@@ -54,7 +93,7 @@ class AtomOp:
 # Single iteration movement
 class MoveOp extends AtomOp:
 	func _init(
-		owner: WeakRef, 
+		owner: Character, 
 		speed: Vector2, # per second
 		duration: float = 1,
 		is_one_shot: bool = true
@@ -71,9 +110,14 @@ class MoveOp extends AtomOp:
 			progress = INF
 			return []
 		if progress == 0.0:
-			owner.get_ref().op_state = CharacterOperationState.ATTACKING
+			owner.animation_state = AnimationState.new(
+				AnimationState.Type.MOVING,
+				owner.move_speed,
+				owner.config.run_anim_names,
+				true
+			)
 		var ops = super.tick(dt, characters)
-		owner.get_ref().position += self.speed * dt
+		owner.position += self.speed * dt
 		if self.is_one_shot:
 			progress = INF
 		
@@ -81,8 +125,8 @@ class MoveOp extends AtomOp:
 
 class AttackOp extends AtomOp:
 	func _init(
-		owner: WeakRef, 
-		target: WeakRef,
+		owner: Character, 
+		target: Character,
 		physical_damage: int,
 		duration: float, # in sec,
 		progress: float = 0.0,
@@ -101,14 +145,12 @@ class AttackOp extends AtomOp:
 			progress = INF
 			return []
 		if progress == 0.0:
-			owner.get_ref().op_state = CharacterOperationState.ATTACKING
+			owner.animation_state = CharacterOperationState.ATTACKING
 		var ops = super.tick(dt, characters)
 		if progress >= 1.0:
-			var target_: Character = self.target.get_ref() as Character
-			if target_ != null:
-				target_.get_damage(self.damage, 0)
+			self.target.get_damage(self.damage, 0)
 		
-		if self.is_recurrent:
+		if self.is_recurrent: # Probably not needed
 			var new_op: AtomOp = AttackOp.new(
 				owner, 
 				self.target, 
@@ -125,6 +167,13 @@ class AttackOp extends AtomOp:
 class CharacterAbility:
 	var cooldown: float # in seconds
 	var cooldown_in_progress: float = 0
+	
+	func _init(
+		cooldown: float,
+		cooldown_in_progress: float = 0
+	):
+		self.cooldown = cooldown
+		self.cooldown_in_progress = cooldown_in_progress
 	
 	func is_suitable(
 		owner: Character,
@@ -149,21 +198,43 @@ class CharacterConfig:
 	var move_speed: float
 	var base_attack_damage: int
 	var attack_range: float
-	var run_anim_names: String
-	var fight_anim: Array[String]
-	var stance_anim: Array[String]
+	var run_anim: String
+	var fight_anims: Array[String]
+	var stance_anims: Array[String]
 	var scene_name: String
 	var abilities: Array[CharacterAbility]
 	var attack_speed: float
 
 
-enum CharacterOperationState {
-	IDLE,
-	MOVING,
-	ATTACKING,
-	SPELLING,
-	KNOCKED
-}
+class AnimationState:
+	enum Type {
+		IDLE,
+		MOVING,
+		ATTACKING,
+		SPELLING,
+		KNOCKED,
+		DIYING
+	}
+	
+	var type: Type
+	var speed: float # from 0 to 2
+	var animationName: String
+	var is_looped: bool
+	
+	func _init(
+		type: Type, 
+		speed: float,
+		animationName: String,
+		is_looped: bool
+	):
+		self.type = type
+		self.speed = speed
+		self.animationName = animationName
+		self.is_looped = is_looped
+		
+	
+	
+	
 
 class Character:
 	var id: int
@@ -176,18 +247,24 @@ class Character:
 	var magic_resistance: int # 0 to 60
 	var attack_damage: int
 	var attack_speed: float
-	var crit_odds: float
+	var move_speed: float
+	var crit_odds: float # TODO: Apply
 	var abilities: Array[CharacterAbility]
 	var position: Vector2
 	
-	signal op_state_changed(old: CharacterOperationState, new: CharacterOperationState)
-	var op_state = CharacterOperationState.IDLE:
+	signal animation_state_changed(old: AnimationState, new: AnimationState)
+	var animation_state = FightEngine.AnimationState.new(
+		AnimationState.Type.IDLE, 
+		0.0,
+		"",
+		false
+	):
 		get:
-			return op_state
+			return animation_state
 		set(value):
-			var old_state = op_state
-			op_state = value
-			op_state_changed.emit(old_state, value)
+			var old_state = animation_state
+			animation_state = value
+			animation_state_changed.emit(old_state, value)
 
 	signal health_changed(old: int, new: int)
 	var current_hp:
