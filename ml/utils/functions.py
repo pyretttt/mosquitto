b11ba201099f6874b40c8214b04b95b159c91118
Semen Bakulin <semen.bakulin@rwb.ru>
2025-07-30 00:06:56 -0700

    iou

--- ml/utils/functions.py
+++ ml/utils/functions.py
@@ -2,9 +2,27 @@ import torch
 
 def get_iou(bbox1, bbox2) -> float:
     """
+    Computes iou of each rectangle in bbox1 with each rectange in bbox2
     :param bbox1: torch.Tensor Nx4    
     :param bbox2: torch.Tensor Mx4
     return: IOU torch.Tensor NxM
     """
+    area1 = (bbox1[..., 2] - bbox1[..., 0]) * (bbox1[..., 3] - bbox1[..., 1]) # (N, )
+    area2 = (bbox2[..., 2] - bbox2[..., 0]) * (bbox2[..., 3] - bbox2[..., 1]) # (M, )
+
+    # Let's find rightmost leading edge between each rectange in first and second array
+    x_left = torch.max(bbox1[..., None, 0], bbox2[..., 0])
+    # Let's find leftmost trailling edge between each rectange in first and second array
+    x_right = torch.min(bbox1[..., None, 2], bbox2[..., 2])
+    # Let's find lower top edge between each rectange in first and second array
+    y_top = torch.max(bbox1[..., None, 1], bbox2[..., 1]) 
+    # Let's find upper bottom edge between each rectange in first and second array
+    y_bottom = torch.min(bbox1[..., None, 4], bbox2[..., 4])
+
+    # If x_right < x_left then there's no intersection.
+    # The same if y_bottom < y_top there's no intesection.
     
-    intersection = 1 # NxM
+    intersection = (x_right - x_left).clamp(min=0.0) * (y_bottom - y_top).clamp(min=0.0) # (N, M)
+    union = area1[:, None] + area2 - intersection # (N, M)
+    iou = intersection / union
+    return iou
