4fac985f520b5548b4ec4cdf490c47b3cf8508d2
Semen Bakulin <semen.bakulin@rwb.ru>
2025-07-31 22:38:17 -0700

    i

--- ml/utils/functions.py
+++ ml/utils/functions.py
@@ -1,6 +1,26 @@
 import torch
 
-def get_iou(bbox1, bbox2) -> float:
+def get_iou(bbox1, bbox2):
+    """Computes bounding intersection over union between two boxes
+
+    Args:
+        bbox1 (torch.tensor): bounding box (4, )
+        bbox2 (torch.tensor): bounding box (4, )
+    """
+    area1 = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    area2 = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+    
+    x_left = torch.max(bbox1[0], bbox2[0])
+    x_right = torch.min(bbox1[2], bbox2[2])
+    y_top = torch.max(bbox1[1], bbox2[1])
+    y_bottom = torch.min(bbox1[3], bbox2[3])
+    
+    intersection = (x_right - x_left).clamp(0) * (y_bottom - y_top).clamp(0)
+    
+    return intersection / (area1 + area2 - intersection)
+    
+
+def get_iou_many_to_many(bbox1, bbox2) -> float:
     """
     Computes iou of each rectangle in bbox1 with each rectange in bbox2
     :param bbox1: torch.Tensor Nx4    
