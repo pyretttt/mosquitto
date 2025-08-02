32ec0518736192a33f2751c55da4a3910efea71c
Semen Bakulin <semen.bakulin@rwb.ru>
2025-08-02 07:16:10 -0700

    faster rcnn

--- ml/object_detection/faster_rcnn/faster_rcnn.py
+++ ml/object_detection/faster_rcnn/faster_rcnn.py
@@ -7,8 +7,22 @@ import math
 ## RPN returns classes and 
 
 class Backbone(nn.Module):
-    def __init__(self, ):
-        
+    def __init__(self):
+        super().__init__()
+        backbone = resnet50(pretrained=True)
+        self.extractor = nn.Sequential(
+            backbone.conv1,
+            backbone.bn1,
+            backbone.relu,
+            backbone.maxpool,
+            backbone.layer1,
+            backbone.layer2,
+            backbone.layer3,
+        )
+
+    def forward(self, x):
+        return self.extractor(x)
+
 
 class RPN(nn.Module):
     def __init__(
@@ -25,3 +39,11 @@ class RCNN(nn.Module):
     ):
         super().__init__()
         self.encoder = encoder
+
+
+if __name__ == "__main__":
+    img = torch.randn(1, 3, 224, 320)
+    backbone = Backbone()
+    out = backbone(img)
+
+    print(out.shape)
