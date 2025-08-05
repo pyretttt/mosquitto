332743e2617628135acd36026da10803a2c28590
Semen Bakulin <semen.bakulin@rwb.ru>
2025-08-05 09:12:23 -0700

    anchors

--- ml/object_detection/faster_rcnn/faster_rcnn.py
+++ ml/object_detection/faster_rcnn/faster_rcnn.py
@@ -47,6 +47,7 @@ class RPN(nn.Module):
         rpn_nms_threshold: float,
         **kwargs
     ):
+        super().__init__()
         self.scales = scales
         self.aspect_ratios = aspect_ratios
         self.iou_pos_threshold = iou_pos_threshold
@@ -110,17 +111,87 @@ class RPN(nn.Module):
 
         shift_y, shift_x = torch.meshgrid(shift_y, shift_x, indexing="ij") 
         # both (H_feat, W_feat)
-        assert shift_x.shape == (W_feat, W_feat) and shift_y.shape == (H_feat, W_feat)
+        assert shift_x.shape == (H_feat, W_feat) and shift_y.shape == (H_feat, W_feat)
         shift_y = shift_y.reshape(-1)
         shift_x = shift_x.reshape(-1)
-        assert shift_x.shape == (W_feat * W_feat) and shift_y.shape == (H_feat * W_feat)
-        shifts = torch.stack((shift_x, shift_y, shift_x, shift_y), dim=0) # (H_feat * W_feat x 4)
-        assert shifts.shape == (W_feat * W_feat, 4)
+        assert shift_x.shape == (H_feat * W_feat, ) and shift_y.shape == (H_feat * W_feat, )
+        shifts = torch.stack((shift_x, shift_y, shift_x, shift_y), dim=1) # (H_feat * W_feat x 4)
+        assert shifts.shape == (H_feat * W_feat, 4)
         anchor_coordinates = shifts[:, None, :] + anchor_coords_about_center[None, :, :]
-        assert anchor_coordinates.shape == (W_feat * W_feat, len(self.scales) * len(self.aspect_ratios), 4)
+        assert anchor_coordinates.shape == (H_feat * W_feat, len(self.scales) * len(self.aspect_ratios), 4)
         anchor_coordinates = anchor_coordinates.reshape(-1, 4)
-        assert anchor_coordinates.shape == (W_feat * W_feat * len(self.scales) * len(self.aspect_ratios), 4)
+        assert anchor_coordinates.shape == (H_feat * W_feat * len(self.scales) * len(self.aspect_ratios), 4)
         return anchor_coordinates
+
+    def generate_anchors_(self, image, feat):
+        r"""
+        Method to generate anchors. First we generate one set of zero-centred anchors
+        using the scales and aspect ratios provided.
+        We then generate shift values in x,y axis for all featuremap locations.
+        The single zero centred anchors generated are replicated and shifted accordingly
+        to generate anchors for all feature map locations.
+        Note that these anchors are generated such that their centre is top left corner of the
+        feature map cell rather than the centre of the feature map cell.
+        :param image: (N, C, H, W) tensor
+        :param feat: (N, C_feat, H_feat, W_feat) tensor
+        :return: anchor boxes of shape (H_feat * W_feat * num_anchors_per_location, 4)
+        """
+        grid_h, grid_w = feat.shape[-2:]
+        image_h, image_w = image.shape[-2:]
+        
+        # For the vgg16 case stride would be 16 for both h and w
+        stride_h = torch.tensor(image_h // grid_h, dtype=torch.int64, device=feat.device)
+        stride_w = torch.tensor(image_w // grid_w, dtype=torch.int64, device=feat.device)
+        
+        scales = torch.as_tensor(self.scales, dtype=feat.dtype, device=feat.device)
+        aspect_ratios = torch.as_tensor(self.aspect_ratios, dtype=feat.dtype, device=feat.device)
+        
+        # Assuming anchors of scale 128 sq pixels
+        # For 1:1 it would be (128, 128) -> area=16384
+        # For 2:1 it would be (181.02, 90.51) -> area=16384
+        # For 1:2 it would be (90.51, 181.02) -> area=16384
+        
+        # The below code ensures h/w = aspect_ratios and h*w=1
+        h_ratios = torch.sqrt(aspect_ratios)
+        w_ratios = 1 / h_ratios
+        
+        # Now we will just multiply h and w with scale(example 128)
+        # to make h*w = 128 sq pixels and h/w = aspect_ratios
+        # This gives us the widths and heights of all anchors
+        # which we need to replicate at all locations
+        ws = (w_ratios[:, None] * scales[None, :]).view(-1)
+        hs = (h_ratios[:, None] * scales[None, :]).view(-1)
+        
+        # Now we make all anchors zero centred
+        # So x1, y1, x2, y2 = -w/2, -h/2, w/2, h/2
+        base_anchors = torch.stack([-ws, -hs, ws, hs], dim=1) / 2
+        base_anchors = base_anchors.round()
+        
+        # Get the shifts in x axis (0, 1,..., W_feat-1) * stride_w
+        shifts_x = torch.arange(0, grid_w, dtype=torch.int32, device=feat.device) * stride_w
+
+        # Get the shifts in x axis (0, 1,..., H_feat-1) * stride_h
+        shifts_y = torch.arange(0, grid_h, dtype=torch.int32, device=feat.device) * stride_h
+        
+        # Create a grid using these shifts
+        shifts_y, shifts_x = torch.meshgrid(shifts_y, shifts_x, indexing="ij")
+        # shifts_x -> (H_feat, W_feat)
+        # shifts_y -> (H_feat, W_feat)
+        
+        shifts_x = shifts_x.reshape(-1)
+        shifts_y = shifts_y.reshape(-1)
+        # Setting shifts for x1 and x2(same as shifts_x) and y1 and y2(same as shifts_y)
+        shifts = torch.stack((shifts_x, shifts_y, shifts_x, shifts_y), dim=1)
+        # shifts -> (H_feat * W_feat, 4)
+        
+        # base_anchors -> (num_anchors_per_location, 4)
+        # shifts -> (H_feat * W_feat, 4)
+        # Add these shifts to each of the base anchors
+        anchors = (shifts.view(-1, 1, 4) + base_anchors.view(1, -1, 4))
+        # anchors -> (H_feat * W_feat, num_anchors_per_location, 4)
+        anchors = anchors.reshape(-1, 4)
+        # anchors -> (H_feat * W_feat * num_anchors_per_location, 4)
+        return anchors
 
 class RCNN(nn.Module):
     def __init__(
@@ -133,10 +204,10 @@ class RCNN(nn.Module):
 
 
 if __name__ == "__main__":
+    # Check RPN
     img = torch.randn(1, 3, 224, 320)
     backbone = Backbone()
     out = backbone(img)
-
     print(out.shape)
     
     vgg = torchvision.models.vgg16(pretrained=False)
@@ -144,6 +215,11 @@ if __name__ == "__main__":
     out = vgg.features[:-1](img)
     print(out.shape)
     
-    x, y, z, w = torch.randn(4, 4)
-    print(x, y, z, w)
-    print(torch.stack((x, y, z, w), dim=0).shape)
+    # Check region proposals
+    rpn = RPN(400, (128, 256), (1, 2), 0, 0, 0)
+    image = torch.randn((3, 2560, 2560))
+    feat = torch.randn((3, 160, 160))
+    my_regions = rpn.generate_anchors(image, feat)
+    orig_regions = rpn.generate_anchors(image, feat)
+    
+    assert my_regions == orig_regions
