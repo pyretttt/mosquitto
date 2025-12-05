73bd21c42c63eaed6be13bc01960071ab9141d10
Semen Bakulin <bakulin.semen@rwb.ru>
2025-12-05 10:00:10 +0300

    i

--- ml/object_detection/detr/model.py
+++ ml/object_detection/detr/model.py
@@ -42,3 +42,69 @@ class TransformerEncoderLayer(torch.nn.Module):
         )
         src = src + self.dropout2(src2)
         return src
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, num_layers, encoder_layer, norm):
+        super().__init__()
+        self.layers = torch.nn.ModuleList([
+            encoder_layer.deep_copy() for _ in range(num_layers)
+        ])
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(
+        self,
+        src,
+        src_mask: Optional[Tensor] = None,
+        src_key_padding_mask: Optional[Tensor] = None,
+        pos: Optional[Tensor] = None
+    ):
+        output = src
+        for layer in self.layers:
+            output = layer(
+                output,
+                src_mask=mask,
+                src_key_padding_mask=src_key_padding_mask,
+                pos=pos
+            )
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output
+
+
+class TransformerDecoderLayer(torch.nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int,
+        dropout: float
+    ):
+        super().__init__()
+        self.self_attn = torch.nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.cross_attn = torch.nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.linear1 = torch.nn.Linear(dim_feedforward, d_model)
+        self.dropout = torch.nn.Dropout(p=dropout)
+        self.linear2 = torch.nn.Linear(d_model, dim_feedforward)
+        self.norm1 = torch.nn.LayerNorm(d_model)
+        self.norm2 = torch.nn.LayerNorm(d_model)
+        self.norm3 = torch.nn.LayerNorm(d_model)
+        self.dropout1 = torch.nn.Dropout(p=dropout)
+        self.dropout2 = torch.nn.Dropout(p=dropout)
+        self.dropout3 = torch.nn.Dropout(p=dropout)
+
+
+    def forward(
+        self,
+        src,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = None,
+        memory_mask: Optional[Tensor] = None,
+        tgt_key_padding_mask: Optional[Tensor] = None,
+        memory_key_padding_mask: Optional[Tensor] = None,
+        pos: Optional[Tensor] = None,
+        query_pos: Optional[Tensor] = None
+    ):
+        # Implement
