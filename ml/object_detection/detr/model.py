ee3dbae13994f1e206f901e07610757046451f13
Semen Bakulin <semen.bakulin@rwb.ru>
2025-12-06 10:49:50 +0300

    detr

--- ml/object_detection/detr/model.py
+++ ml/object_detection/detr/model.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 import torch
+from torch import Tensor
+
 
 class TransformerEncoderLayer(torch.nn.Module):
     def __init__(
@@ -44,7 +48,7 @@ class TransformerEncoderLayer(torch.nn.Module):
         return src
 
 
-class Encoder(torch.nn.Module):
+class TransformerEncoder(torch.nn.Module):
     def __init__(self, num_layers, encoder_layer, norm):
         super().__init__()
         self.layers = torch.nn.ModuleList([
@@ -64,7 +68,7 @@ class Encoder(torch.nn.Module):
         for layer in self.layers:
             output = layer(
                 output,
-                src_mask=mask,
+                src_mask=src_mask,
                 src_key_padding_mask=src_key_padding_mask,
                 pos=pos
             )
@@ -98,7 +102,7 @@ class TransformerDecoderLayer(torch.nn.Module):
 
     def forward(
         self,
-        src,
+        tgt,
         memory: Tensor,
         tgt_mask: Optional[Tensor] = None,
         memory_mask: Optional[Tensor] = None,
@@ -108,3 +112,131 @@ class TransformerDecoderLayer(torch.nn.Module):
         query_pos: Optional[Tensor] = None
     ):
         # Implement
+        tgt2 = self.self_attn(tgt + pos, tgt + pos, tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+        tgt2 = self.cross_attn(
+            query_pos + tgt,
+            pos + memory,
+            memory,
+            attn_mask=memory_mask,
+            key_padding_mask=memory_key_padding_mask
+        )
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+        tgt2 = self.linear2(
+            self.dropout(
+                torch.function.relu(
+                    self.linear1(tgt)
+                )
+            )
+        )
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt
+
+
+class TransformerDecoder(torch.nn.Module):
+    def __init__(self, decoder_layer, num_layers, norm=None, return_intermediate=False):
+        super().__init__()
+        self.layers = torch.nn.ModuleList([
+            decoder_layer.deep_copy() for _ in range(num_layers)
+        ])
+        self.num_layers = num_layers
+        self.norm = norm
+        self.return_intermediate = return_intermediate
+
+    def forward(
+        self,
+        tgt,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = None,
+        memory_mask: Optional[Tensor] = None,
+        tgt_key_padding_mask: Optional[Tensor] = None,
+        memory_key_padding_mask: Optional[Tensor] = None,
+        pos: Optional[Tensor] = None,
+        query_pos: Optional[Tensor] = None
+    ):
+        tgt = tgt
+        intermediate = []
+        for layer in self.layers:
+            tgt = layer(
+                tgt,
+                memory,
+                tgt_mask,
+                memory_mask,
+                tgt_key_padding_mask,
+                memory_key_padding_mask,
+                pos,
+                query_pos
+            )
+        if self.return_intermediate:
+            intermediate.append(tgt)
+
+        if self.norm is not None:
+            tgt = self.norm(tgt)
+            if self.return_intermediate:
+                intermediate.pop()
+                intermediate.append(tgt)
+
+        if self.return_intermediate:
+            return torch.stack(intermediate)
+
+        return tgt.unsqueeze(0)
+
+
+class Transformer(torch.nn.Module):
+    def __init__(
+        self,
+        d_model: int = 512,
+        dim_feedforward: int = 2048,
+        nhead: int = 8,
+        num_encoder_layer: int = 6,
+        num_decoder_layers: int = 6,
+        dropout=0.1,
+        return_intermediates=False
+    ):
+        super().__init__()
+        encoder_norm = torch.nn.LayerNorm(d_model)
+        self.encoder = TransformerEncoder(
+            num_layers=num_encoder_layer,
+            encoder_layer=TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, dropout=dropout),
+            norm=encoder_norm
+        )
+        decoder_norm = torch.nn.LayerNorm(d_model)
+        self.decoder = TransformerDecoder(
+            num_layers=num_decoder_layers,
+            encoder_layer=TransformerDecoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, dropout=dropout),
+            norm=decoder_norm
+        )
+        self._reset_parameters()
+        self.return_intermediates = return_intermediates
+
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                torch.nn.init.xavier_uniform_(p)
+
+
+    def forward(self, src, mask, query_embed, pos_embed):
+        bs, c, h, w = src.shape
+        src = src.flatten(2).permute(2, 0, 1) # H*W, N, C
+        pos_embed = pos_embed.flatten(2).permute(2, 0, 1) # # H*W, N, C
+        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
+        mask = mask.flatten(1)
+
+        memory = self.encoder(
+            src=src,
+            src_key_padding_mask=mask,
+            pos=pos_embed
+        )
+        tgt = torch.zeros_like(query_embed)
+        hs = self.decoder.forward(
+            tgt=tgt,
+            memory=memory,
+            memory_key_padding_mask=mask,
+            pos=pos_embed,
+            query_pos=query_embed
+        )
+        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
