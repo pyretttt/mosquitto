a03b5c90ef734b0c8296cfc9c9f4e245a55ff090
Semen Bakulin <semen.bakulin@rwb.ru>
2025-12-06 19:03:06 +0300

    i

---
+++ ml/object_detection/detr_fork/model/ab.py
@@ -0,0 +1,8 @@
+import torch
+
+def get_spatial_position_embedding(emb_dim, feat_map):
+    H, W = feat_map.shape[2], feat_map.shape[3]
+    vertical_indices = torch.arange(H, dtype=torch.float32, device=feat_map.device)
+    horizontal_indices = torch.arange(W, dtype=torch.float32, device=feat_map.device)
+    grid = torch.meshgrid(vertical_indices, horizontal_indices, indexing="ij")
+    grid = torch.stack(grid, dim=0) # 2, 
