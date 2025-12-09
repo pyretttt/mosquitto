be8bbe7c4ed6cae0deb81140be8aae57018f1afd
Semen Bakulin <bakulin.semen@rwb.ru>
2025-12-09 10:01:47 +0300

    i

--- ml/object_detection/detr_fork/model/ab.py
+++ ml/object_detection/detr_fork/model/ab.py
@@ -1,5 +1,5 @@
 import torch
-
+import numpy as np
 
 def get_spatial_position_embedding(emb_dim, feat_map):
     H, W = feat_map.shape[2], feat_map.shape[3]
@@ -11,27 +11,27 @@ def get_spatial_position_embedding(emb_dim, feat_map):
     grid_w = grid[1].reshape(-1).unsqueeze(1) # H * W, 1
 
     factor = torch.exp(
-        -torch.log(10000)
-        * torch.arange(0, emb_dim, 2, device=feat_map.device).float()
-        / emb_dim
+        -np.log(10000)
+        * torch.arange(0, emb_dim, 4, device=feat_map.device).float()
+        / (emb_dim // 4)
     ) # (emb_dim // 2)
 
-    grid_h_emb = grid_h * factor # H * W, embed_dim // 2
-    grid_w_emb = grid_w * factor # H * W, embed_dim // 2
+    grid_h_emb = grid_h * factor # H * W, embed_dim // 4
+    grid_w_emb = grid_w * factor # H * W, embed_dim // 4
 
-    grid_h_emb = torch.cat([
+    pos_h = torch.cat([
         torch.sin(grid_h_emb),
         torch.cos(grid_h_emb)
-    ], dim=-1) # H * W, embed_dim
+    ], dim=-1) # H * W, embed_dim // 2
 
-    grid_w_emb = torch.cat([
+    pos_w = torch.cat([
         torch.sin(grid_w_emb),
         torch.cos(grid_w_emb)
+    ], dim=-1) # H * W, embed_dim // 2
+
+    pos = torch.cat([
+        pos_h,
+        pos_w
     ], dim=-1) # H * W, embed_dim
 
-    pos_emb = torch.cat([
-        grid_h_emb,
-        grid_w_emb
-    ], dim=-1) # H * W, embed_dim
-
-    return pos_emb
+    return pos
