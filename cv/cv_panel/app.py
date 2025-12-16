47ef686d44967fd71f9fbe77cd29fa2046dac00a
Semen Bakulin <semen.bakulin@rwb.ru>
2025-12-16 19:30:34 +0300

    i

--- cv/cv_panel/app.py
+++ cv/cv_panel/app.py
@@ -1,7 +1,8 @@
 import sys
 
-from PySide6.QtCore import Qt
+
 from PySide6.QtGui import QAction
+from PySide6.QtCore import Qt, QAbstractListModel, QModelIndex, QSize
 from PySide6.QtWidgets import (
     QApplication,
     QWidget,
@@ -15,6 +16,24 @@ from PySide6.QtWidgets import (
     QStatusBar
 )
 
+class MainScreen:
+    class MainWindow(QMainWindow):
+        def __init__(self, ):
+            super().__init__()
+            self.setup_ui()
+
+        def setup_ui(self):
+            self.setWindowTitle("List of algorithms")
+
+            self.layout = QStackedLayout()
+            self.toolbar = QToolBar("My main toolbar")
+            self.addToolBar(self.toolbar)
+
+
+    class Data(QAbstractListModel):
+        def __init__(self, )
+
+
 class MainWindow(QMainWindow):
     def __init__(self, ):
         super().__init__()
@@ -22,27 +41,10 @@ class MainWindow(QMainWindow):
 
     def setup_ui(self):
         self.setWindowTitle("List of algorithms")
-        
+
         self.layout = QStackedLayout()
         self.toolbar = QToolBar("My main toolbar")
         self.addToolBar(self.toolbar)
-
-        self.button_action = QAction("Your button", self)
-        self.button_action.setStatusTip("This is your button")
-        self.button_action.triggered.connect(self.toolbar_button_clicked)
-        self.button_action.setCheckable(True)
-        self.toolbar.addAction(self.button_action)
-
-
-        self.setStatusBar(QStatusBar(self))
-
-
-
-
-    def toolbar_button_clicked(self, s):
-        print("click", s)
-
-
 
 
 def main():
