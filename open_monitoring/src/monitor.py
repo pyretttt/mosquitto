a7fb4c67155d3d87b4b6b58b6438423d6080fc36
bakulin.semen <bakulin.semen@rwb.ru>
2026-04-06 16:23:49 +0300

    I

--- open_monitoring/src/monitor.py
+++ open_monitoring/src/monitor.py
@@ -15,6 +15,26 @@ class Notify(BaseModel):
 
 
 class Monitor(BaseModel):
+    """
+    Should be represented as:
+    {
+        "name": "APPL stocks",
+        "pull": {
+            "lib": "obb",
+            "method": "equity.price.quote",
+            "params": {
+                "provider": "yfinance",
+                "symbol": "AAPL"
+            }
+        },
+        "condition": "data.last_price < 150 || data.change_percent > 5.0",
+        "notify": {
+            "telegram": {
+                "template": "AAPL alert: price=${data.last_price}"
+            }
+        }
+    }
+    """
     name: str
     pull: Pull
     condition: str
