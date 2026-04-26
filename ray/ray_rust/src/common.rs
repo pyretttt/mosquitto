71896e1140e908b30c5941709ee7f7e3f37d203b
Semen Bakulin <semen.bakulin@rwb.ru>
2026-04-26 06:01:08 +0300

    depth stencil

--- ray/ray_rust/src/common.rs
+++ ray/ray_rust/src/common.rs
@@ -141,6 +141,7 @@ pub struct State {
     camera_buffer: wgpu::Buffer,
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
+    depth_texture: crate::texture::Texture,
 }
 
 impl State {
@@ -319,6 +320,8 @@ impl State {
             }
         );
 
+        let depth_texture = crate::texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
@@ -359,7 +362,13 @@ impl State {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: crate::texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: Some(true),
+                depth_compare: Some(wgpu::CompareFunction::Less), // 1.
+                stencil: wgpu::StencilState::default(), // 2.
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -390,7 +399,7 @@ impl State {
             camera_uniform,
             instances,
             instance_buffer,
-
+            depth_texture,
         })
     }
 
@@ -398,6 +407,7 @@ impl State {
         if width > 0 && height > 0 {
             self.config.width = width;
             self.config.height = height;
+            self.depth_texture = crate::texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
             self.surface.configure(&self.device, &self.config);
             self.is_surface_configured = true;
         }
@@ -465,8 +475,15 @@ impl State {
                         },
                     })
                 ],
-                depth_stencil_attachment: None,
-                occlusion_query_set: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store,
+                    }),
+                    stencil_ops: None,
+                }),
+                            occlusion_query_set: None,
                 timestamp_writes: None,
                 multiview_mask: None,
             });
