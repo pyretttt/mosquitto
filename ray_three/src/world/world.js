0557ac4af21616996c2bb7ae28c1cbaf08d9aebc
Semen Bakulin <semen.bakulin@rwb.ru>
2026-06-03 05:25:09 +0300

    groups

--- ray_three/src/world/world.js
+++ ray_three/src/world/world.js
@@ -3,6 +3,7 @@ import { createCube } from './components/cube.js';
 import { createScene } from './components/scene.js';
 import { createLights } from './components/lights.js';
 import { createControls } from './systems/control.js';
+import { createMeshGroup } from './components/group.js';
 
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/resizer.js';
@@ -20,23 +21,14 @@ class World {
         container.append(this.renderer.domElement);
 
         this.controls = createControls(this.camera, this.renderer.domElement);
-
-        const cube = createCube();
-        cube.update = (delta) => {
-            // cube.rotation.z +=  0.5238 * delta;
-            // cube.rotation.x += 0.5238 * delta;
-            // cube.rotation.y += 0.5238 * delta;
-        };
         this.camera.update = (delta) => {
             this.camera.updateProjectionMatrix();
         };
-        this.loop.updatables.push(cube, this.camera, this.controls);
         const { directionalLight, ambientLight } = createLights();
+        const meshGroup = createMeshGroup();
 
-        this.scene.add(cube, directionalLight, ambientLight);
-
-        this.controls.target.copy(cube.position);
-
+        this.loop.updatables.push(meshGroup, this.camera, this.controls);
+        this.scene.add(meshGroup, directionalLight, ambientLight);
 
         this.resizer = new Resizer(container, this.camera, this.renderer);
         this.resizer.onResize = () => {
