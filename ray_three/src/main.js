28ddf7e18dd527924307495cd6abacfaec5e8fc8
Semen Bakulin <semen.bakulin@rwb.ru>
2026-06-05 05:35:17 +0300

    render scene

--- ray_three/src/main.js
+++ ray_three/src/main.js
@@ -1,19 +1,10 @@
-import {
-    BoxGeometry,
-    Color,
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-} from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import { PerspectiveRenderUseCase } from './world/render.js';
 
 async function main() {
-    const vampire_scene = await new GLTFLoader().loadAsync("asset/jill_vampire/scene.gltf");
-    const render_use_case = new PerspectiveRenderUseCase(vampire_scene);
+    const gltf = await new GLTFLoader().loadAsync('asset/jill_vampire/scene.gltf');
+    const render_use_case = new PerspectiveRenderUseCase(gltf.scene);
 
     render_use_case.start();
 }
--- ray_three/src/world/render.js
+++ ray_three/src/world/render.js
@@ -1,4 +1,5 @@
-import { PerspectiveCamera, WebGLRenderer, Timer } from 'three';
+import { PerspectiveCamera, WebGLRenderer, Timer, Scene, Color } from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 function resize(camera, renderer, canvas) {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -9,28 +10,32 @@ function resize(camera, renderer, canvas) {
 }
 
 class RenderController {
-    constructor(pipeline, canvas) {
+    constructor(pipeline, canvas, orbitControls) {
         this.pipeline = pipeline;
         this.isRendering = true;
         this.timer = new Timer();
         this.canvas = canvas;
+        this.orbitControls = orbitControls;
 
         this.updatables = [];
+        resize(this.pipeline.camera, this.pipeline.renderer, canvas);
         window.addEventListener('resize', () => {
-            resize(this.pipeline.camera, this.pipeline.renderer, canvas);
+            resize(this.pipeline.camera, this.pipeline.renderer, this.canvas);
         });
     }
 
     startRenderLoop() {
         this.pipeline.renderer.setAnimationLoop(() => {
-            while (this.isRendering) {
-                this.timer.update();
-                this.updatables.forEach(
-                    updatable => updatable.update(this.timer.getDelta())
-                );
-                this.pipeline.render();
+            if (!this.isRendering) {
+                this.pipeline.renderer.setAnimationLoop(null);
+                return;
             }
-            this.pipeline.renderer.setAnimationLoop(null);
+
+            this.timer.update();
+            this.updatables.forEach(
+                updatable => updatable.updateTick(this.timer.getDelta())
+            );
+            this.pipeline.render();
         });
     }
 
@@ -53,25 +58,37 @@ class RenderPipeline {
 
 class PerspectiveRenderUseCase {
     constructor(scene) {
-        const canvas = document.querySelector('#scene-container').domElement;
+        const rootScene = new Scene();
+        rootScene.add(scene);
+        rootScene.background = new Color('red');
+
+        const canvas = document.querySelector('#scene-container');
         const renderer = new WebGLRenderer({
-            canvas: canvas,
             antialias: true,
             depth: true,
         });
+        canvas.append(renderer.domElement);
+
+        const camera = new PerspectiveCamera(
+            75,
+            16/9,
+            0.1,
+            1000
+        );
+        camera.position.set(0, 0, 10);
+        const orbitControls = new OrbitControls(camera, renderer.domElement);
+        orbitControls.updateTick = (delta) => {
+            orbitControls.update();
+        };
         renderer.physicallyCorrectLights = true;
         this.renderPipeline = new RenderPipeline(
-            new PerspectiveCamera(
-                75,
-                16/9,
-                0.1,
-                1000
-            ),
-            scene,
+            camera,
+            rootScene,
             renderer
         );
 
-        this.controller = new RenderController(this.renderPipeline, canvas);
+        this.controller = new RenderController(this.renderPipeline, canvas, orbitControls);
+        this.controller.updatables.push(orbitControls);
     }
 
     start() {
