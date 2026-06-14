910e30f4e1ec5bb47861ad8f1f24b585a71640a7
Semen Bakulin <semen.bakulin@rwb.ru>
2026-06-14 16:51:28 +0300

    k8s working

--- oci-infra/k8s/k8s-cluster.tf
+++ oci-infra/k8s/k8s-cluster.tf
@@ -53,20 +53,54 @@ resource "kubernetes_deployment_v1" "nginx_deployment" {
   }
 }
 
-# resource "kubernetes_service" "nginx_service" {
-#   metadata {
-#     name      = "nginx-service"
-#     namespace = kubernetes_namespace.core_namespace.id
-#   }
-#   spec {
-#     selector = {
-#       app = "nginx"
-#     }
-#     port {
-#       port        = 80
-#       target_port = 80
-#       node_port   = 31600
-#     }
-#     type = "NodePort"
-#   }
-# }
+resource "kubernetes_service" "nginx_service" {
+  metadata {
+    name      = "nginx-service"
+    namespace = kubernetes_namespace.core_namespace.id
+  }
+  spec {
+    selector = {
+      app = "nginx"
+    }
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 31600
+    }
+    type = "NodePort"
+  }
+}
+
+resource "oci_network_load_balancer_network_load_balancer" "nlb" {
+  compartment_id = var.compartment_id
+  display_name   = "k8s-nlb"
+  subnet_id      = var.public_subnet_id
+  is_private                     = false
+  is_preserve_source_destination = false
+}
+
+resource "oci_network_load_balancer_backend_set" "nlb_backend_set" {
+  health_checker {
+    protocol = "TCP"
+    port     = 10256
+  }
+  name                     = "k8s-backend-set"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.nlb.id
+  policy                   = "FIVE_TUPLE"
+  is_preserve_source = false
+}
+
+resource "oci_network_load_balancer_backend" "nlb_backend" {
+  count                    = length(local.active_nodes)
+  backend_set_name         = oci_network_load_balancer_backend_set.nlb_backend_set.name
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.nlb.id
+  port                     = 31600
+  target_id                = local.active_nodes[count.index].id
+}
+resource "oci_network_load_balancer_listener" "nlb_listener" {
+  default_backend_set_name = oci_network_load_balancer_backend_set.nlb_backend_set.name
+  name                     = "k8s-nlb-listener"
+  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.nlb.id
+  port                     = "80"
+  protocol                 = "TCP"
+}
