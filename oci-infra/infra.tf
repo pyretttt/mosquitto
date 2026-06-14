6541485d4516ec74165f915090b03f118c522e2c
Semen Bakulin <semen.bakulin@rwb.ru>
2026-06-14 10:53:57 +0300

    k8s

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -109,3 +109,58 @@ resource "oci_core_subnet" "vcn_public_subnet" {
 }
 
 # Kubernetes Cluster
+resource "oci_containerengine_cluster" "k8s_cluster" {
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.34.2"
+  name               = "k8s-cluster"
+  vcn_id             = module.vcn.vcn_id
+  endpoint_config {
+    is_public_ip_enabled = true # allows public access to the cluster
+    subnet_id            = oci_core_subnet.vcn_public_subnet.id # public subnet
+  }
+  options {
+    add_ons {
+      is_kubernetes_dashboard_enabled = true
+      is_tiller_enabled               = false
+    }
+    kubernetes_network_config {
+      pods_cidr     = "10.244.0.0/16"
+      services_cidr = "10.96.0.0/16"
+    }
+    service_lb_subnet_ids = [oci_core_subnet.vcn_public_subnet.id]
+  }
+}
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+# define node pool for k8s cluster
+resource "oci_containerengine_node_pool" "k8s_node_pool" {
+  cluster_id         = oci_containerengine_cluster.k8s_cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.34.2"
+  name               = "k8s-node-pool"
+  node_config_details {
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    }
+    size = 2 # 2 nodes in the node pool
+  }
+  node_shape = "VM.Standard.A1.Flex"
+  node_shape_config {
+    memory_in_gbs = 6
+    ocpus         = 1
+  }
+  node_source_details {
+    # https://docs.oracle.com/en-us/iaas/images/oracle-linux-9x/oracle-linux-9-7-aarch64-2026-05-28-0.htm
+    image_id    = "ocid1.image.oc1.me-dubai-1.aaaaaaaa2zcql3hcgbvmjzzaxyakhxhd3kbhmo4ysuf3lclz7pepjgh62idq"
+    source_type = "image"
+  }
+  initial_node_labels {
+    key   = "name"
+    value = "k8s-cluster"
+  }
+  ssh_public_key = var.ssh_public_key
+}
