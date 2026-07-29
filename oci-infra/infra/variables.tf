7881c16ac0af5fc469b86fbfedafb31d8009bab7
Semen Bakulin <semen.bakulin@rwb.ru>
2026-07-29 16:57:36 +0400

    bla

--- oci-infra/infra/variables.tf
+++ oci-infra/infra/variables.tf
@@ -1,14 +1,37 @@
 variable "compartment_id" {
-    type = string
-    description = "The compartment to create the resources in"
+  type        = string
+  description = "The compartment to create the resources in"
+  default     = "ocid1.tenancy.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 }
-
 variable "region" {
-    type = string
-    description = "The region to create the resources in"
+  type        = string
+  description = "The region to provision the resources in"
+  default     = "eu-frankfurt-1"
 }
-
 variable "ssh_public_key" {
-    type = string
-    description = "The SSH public key to use for connecting to the worker nodes"
+  type        = string
+  description = "The SSH public key to use for connecting to the worker nodes"
+  default     = "ssh-rsa AAAA........"
+}
+variable "bastion_allowed_ips" {
+  type        = list(string)
+  description = "List of IP prefixes allowed to connect via bastion"
+  default     = ["127.0.0.1/32"]
+}
+variable "ad_list" {
+  type        = list
+  description = "List of length 2 with the names of availability regions to use"
+  default     = ["QZUh:ME-DUBAI-1-AD-1", "QZUh:ME-DUBAI-1-AD-1"] # dubai taken from create instance page
+}
+variable "git_token" {
+  description = "Git PAT"
+  sensitive   = true
+  type        = string
+  default     = null
+}
+variable "git_url" {
+  description = "Git repository URL"
+  default     = "https://github.com/OWNER/REPO"
+  type        = string
+  nullable    = false
 }
