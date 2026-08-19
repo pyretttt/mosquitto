7cb666c03b7fe695e43c09255f84093d2ba1d0f4
Semen Bakulin <semen.bakulin@rwb.ru>
2026-08-19 20:06:47 +0400

    py

--- polywhale/py/src/screens/__init__.py
+++
@@ -1,5 +0,0 @@
-from screens.dashboard import DashboardScreen
-from screens.intro import IntroScreen
-from screens.settings import SettingsScreen
-
-__all__ = ["DashboardScreen", "IntroScreen", "SettingsScreen"]
